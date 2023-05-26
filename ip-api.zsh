6bc63be6ce25cb23d21c06f029ee05b1116abcea
Tibor <36704519+tibor3741@users.noreply.github.com>
2023-05-26 18:07:41 +0200

    space added ip-api.zsh

--- ip-api.zsh
+++ ip-api.zsh
@@ -17,22 +17,22 @@ ipapi_info() {
   local mobile=$(echo "$result" | jq -r '.mobile')
   local is_proxy=$(echo "$result" | jq -r '.proxy')
 
-tput setaf 46; echo -n "🌐 Status: "; tput sgr0; echo "$status_code"
-tput setaf 46; echo -n "🔁 Reverse DNS: "; tput sgr0; echo "$reverse"
-tput setaf 46; echo -n "🏳️Country: "; tput sgr0; echo "$country"
-tput setaf 46; echo -n "🌍 Country Code:";tput sgr0;echo "$countryCode"
-tput setaf 46; echo -n "📍 Region:"; tput sgr0; echo "$region"
-tput setaf 46; echo -n "🏞️  Region Name: "; tput sgr0; echo "$regionName"
-tput setaf 46; echo -n "🏙️  City: "; tput sgr0; echo "$city"
-tput setaf 46; echo -n "📪 Zip Code:"; tput sgr0; echo "$zip"
-tput setaf 46; echo -n "🕒 Timezone: "; tput sgr0; echo "$timezone"
-tput setaf 46; echo -n "🔌 ISP: "; tput sgr0; echo "$isp"
-tput setaf 46; echo -n "🏢 Organization:"; tput sgr0; echo "$org"
-tput setaf 46; echo -n "🕸️  Hosting:";tput sgr0; echo "$hosting"
-tput setaf 46; echo -n "📱 Mobile: ";tput sgr0; echo "$mobile"
-tput setaf 46; echo -n  "📎 Proxy: ";tput sgr0; echo  "$is_proxy"
-tput setaf 46; echo -n "🔒 AS: "; tput sgr0; echo  "$as"
-tput setaf 46; echo -n "🔎 IP: "; tput sgr0; echo  "$query"
+tput setaf 46; echo -n " 🌐 Status: "; tput sgr0; echo "$status_code"
+tput setaf 46; echo -n " 🔁 Reverse DNS: "; tput sgr0; echo "$reverse"
+tput setaf 46; echo -n " 🏳️ Country: "; tput sgr0; echo "$country"
+tput setaf 46; echo -n " 🌍 Country Code:";tput sgr0;echo "$countryCode"
+tput setaf 46; echo -n " 📍 Region:"; tput sgr0; echo "$region"
+tput setaf 46; echo -n " 🏞️  Region Name: "; tput sgr0; echo "$regionName"
+tput setaf 46; echo -n " 🏙️  City: "; tput sgr0; echo "$city"
+tput setaf 46; echo -n " 📪 Zip Code:"; tput sgr0; echo "$zip"
+tput setaf 46; echo -n " 🕒 Timezone: "; tput sgr0; echo "$timezone"
+tput setaf 46; echo -n " 🔌 ISP: "; tput sgr0; echo "$isp"
+tput setaf 46; echo -n " 🏢 Organization:"; tput sgr0; echo "$org"
+tput setaf 46; echo -n " 🕸️  Hosting:";tput sgr0; echo "$hosting"
+tput setaf 46; echo -n " 📱 Mobile: ";tput sgr0; echo "$mobile"
+tput setaf 46; echo -n  " 📎 Proxy: ";tput sgr0; echo  "$is_proxy"
+tput setaf 46; echo -n " 🔒 AS: "; tput sgr0; echo  "$as"
+tput setaf 46; echo -n " 🔎 IP: "; tput sgr0; echo  "$query"
 
 }
 
